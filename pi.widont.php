8f4a01d0e3ab284f89228a245db8bb512ad99f14
John Henry Donovan <johnhenry@redmoonmedia.com>
2012-08-04 02:40:56 +0100

    first commit
    Signed-off-by: John Henry Donovan <johnhenry@redmoonmedia.com>

---
+++ pi.widont.php
@@ -0,0 +1,24 @@
+<?php
+class Plugin_widont extends Plugin {
+
+  var $meta = array(
+    'name'       => 'Widont',
+    'version'    => '1.0',
+    'author'     => 'John Henry Donovan',
+    'author_url' => 'http://johnhenry.ie'
+  );
+
+  public function index() {
+
+
+    Statamic_View::$_dataStore = array_merge(Statamic_View::$_dataStore, $this->attributes);
+    $this->content = $this->parser->parse($this->content, Statamic_View::$_dataStore, 'Statamic_View::callback');
+    
+    $this->content = preg_replace('/\s+(\S+)$/', '&#160;$1', rtrim($this->content));
+   
+    return $this->content;
+  }
+
+ 
+    
+}
